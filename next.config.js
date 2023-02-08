6e016f144ec9c15302b37278047b0c035cdd8a58
Roy Rivera <roy.fita23@gmail.com>
2023-02-08 11:25:05 -0600

    eslint formatting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  compiler: {
+    styledComponents: true,
+  },
   reactStrictMode: true,
+  swcMinify: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: { and: [/\.(js|ts|md)x?$/] },
+      use: [
+        {
+          loader: '@svgr/webpack',
+        },
+      ],
+    })
+    return config
+  },
 }
 
 module.exports = nextConfig
