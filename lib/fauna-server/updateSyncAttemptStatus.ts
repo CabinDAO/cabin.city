9ee5193aab876bd9f3d78e54a15dc5d6ce3c00c5
Sean Connolly <sean@seanconnolly.dev>
2023-02-15 09:34:57 -0700

    Ignore status updates if already successful (#7)

--- lib/fauna-server/updateSyncAttemptStatus.ts
+++ lib/fauna-server/updateSyncAttemptStatus.ts
@@ -5,18 +5,29 @@ export const updateSyncAttemptStatus = async (ref: Expr, status: string) => {
   return faunaServerClient.query(
     q.Let(
       {
-        failedAttemptCount: q.If(
-          q.Equals(status, 'Failed'),
-          q.Add(q.Select(['data', 'failedAttemptCount'], q.Get(ref), 0), 1),
-          0
-        ),
+        syncAttempt: q.Get(ref),
       },
-      q.Update(ref, {
-        data: {
-          status,
-          failedAttemptCount: q.Var('failedAttemptCount'),
-        },
-      })
+      q.If(
+        // If the status is already 'Successful', don't update it
+        // This is likely caused by a duplicate transaction reverting
+        q.Not(q.Equals(status, 'Successful')),
+        q.Let(
+          {
+            failedAttemptCount: q.If(
+              q.Equals(status, 'Failed'),
+              q.Add(q.Select(['data', 'failedAttemptCount'], q.Get(ref), 0), 1),
+              0
+            ),
+          },
+          q.Update(ref, {
+            data: {
+              status,
+              failedAttemptCount: q.Var('failedAttemptCount'),
+            },
+          })
+        ),
+        null
+      )
     )
   )
 }
