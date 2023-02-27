1e6fb529deab893360b1f92d1dfcd3542e731235
Sean Connolly <sean@seanconnolly.dev>
2023-02-27 07:41:31 -0700

    Clear apollo state when logging out (#23)

--- pages/logout.tsx
+++ pages/logout.tsx
@@ -3,12 +3,14 @@ import { useRouter } from 'next/router'
 
 import type { NextPage } from 'next'
 import { logOut } from '@/lib/auth/logout'
+import { apolloClient } from '@/lib/apollo/apollo-client'
 
 const LogoutPage: NextPage = () => {
   const router = useRouter()
 
   useEffect(() => {
     logOut().then(() => {
+      apolloClient.clearStore()
       router.push('/login')
     })
   }, [router])
