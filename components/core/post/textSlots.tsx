e92f05edd7a333e0a862b8976ee72a2c3fa2a5fb
Roy Rivera <roy.fita23@gmail.com>
2023-04-12 10:16:52 -0600

    Fixing super long string (#115)

--- components/core/post/textSlots.tsx
+++ components/core/post/textSlots.tsx
@@ -1,14 +1,27 @@
 import { Body1 } from '../Typography'
 import { PostProps } from './Post'
 import { PostSlots } from './post-slots'
+import styled from 'styled-components'
 
 const TextContent = (props: PostProps) => {
   const { activityItem } = props
   const { activity } = activityItem
 
-  return <Body1>{activity.text}</Body1>
+  return <StyledBody>{activity.text}</StyledBody>
 }
 
 export const textSlots: PostSlots = {
   Content: TextContent,
 }
+
+const StyledBody = styled(Body1)`
+  overflow-wrap: break-word;
+
+  -ms-word-break: break-all;
+  word-break: break-word;
+
+  -ms-hyphens: auto;
+  -moz-hyphens: auto;
+  -webkit-hyphens: auto;
+  hyphens: auto;
+`
