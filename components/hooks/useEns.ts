bc49a533a0f22bdd3bf6a8491997d9d02809ae02
Roy Rivera <roy.fita23@gmail.com>
2023-05-15 21:38:48 -0600

    Display ENS (#201)

--- components/hooks/useEns.ts
+++ components/hooks/useEns.ts
@@ -1,23 +1,20 @@
 import { useEffect, useCallback, useState } from 'react'
-import { getProvider } from '@wagmi/core'
-import { useNetwork } from 'wagmi'
+import { getAlchemyProvider } from '@/lib/alchemy'
 
 const useEns = (address: string | undefined) => {
   const [ens, setEns] = useState<string | null>(null)
-  const { chain } = useNetwork()
 
   const fetchEns = useCallback(async () => {
     try {
-      const provider = getProvider()
+      const provider = getAlchemyProvider('mainnet')
       if (!address) return
-      if (['homestead', 'goerli'].includes(chain?.network || '')) {
-        const _ens = await provider.lookupAddress(address)
-        setEns(_ens)
-      }
+
+      const _ens = await provider.lookupAddress(address)
+      setEns(_ens)
     } catch (e) {
       console.error(e)
     }
-  }, [address, chain])
+  }, [address])
 
   useEffect(() => {
     fetchEns()
