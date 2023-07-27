b7cd2e896bc8883d29bf293c7e97209733777d9c
Alex Grintsvayg <grin@cabin.city>
2023-07-27 12:37:19 -0400

    temp fix for .env parsing bug

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
+if (process.env.NODE_ENV == 'development') {
+  require('dotenv').config({ override: true }) // this is a hack but it works.
+  // the bug is that an old dotenv is somehow loading these values first, and then the new dotenv is not overriding them
+  // old dotenv might be coming from @next/env or whatever builtin shit nextjs is doing
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   compiler: {
