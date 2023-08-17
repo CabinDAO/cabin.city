63184d8666697cedf76e7142eb56b461541dc3eb
Alex Grintsvayg <grin@cabin.city>
2023-08-17 10:48:53 -0400

    add fauna permissions for admins

--- fauna/roles/predicates/writeOnlyBy.ts
+++ fauna/roles/predicates/writeOnlyBy.ts
@@ -2,5 +2,17 @@ import { query as q } from 'faunadb'
 
 export const writeOnlyBy = (fieldKey: string) =>
   q.Query((oldData, newData, ref) =>
-    q.Equals(q.CurrentIdentity(), q.Select(['data', fieldKey], q.Get(ref)))
+    q.Let(
+      {
+        canWrite: q.Equals(
+          q.CurrentIdentity(),
+          q.Select(['data', fieldKey], q.Get(ref))
+        ),
+        isAdmin: q.Equals(
+          q.Select(['data', 'isAdmin'], q.Get(q.CurrentIdentity()), false),
+          true
+        ),
+      },
+      q.Or(q.Var('canWrite'), q.Var('isAdmin'))
+    )
   )
