9444343b6455b44cf22c5c73ae4ca39f5db64cf1
Alex Grintsvayg <grin@cabin.city>
2023-10-30 02:38:29 -0400

    hack to make grin's profile public for tns demo

--- components/profile/view-profile/ProfileContent.tsx
+++ components/profile/view-profile/ProfileContent.tsx
@@ -22,7 +22,7 @@ export const ProfileContent = ({
   activityItems,
 }: ProfileContentProps) => {
   const { user: me } = useProfile()
-  if (!me) return null
+  if (!me && profile._id !== '361917941542813776') return null
 
   const isOwnProfile = me?._id === profile._id
 
