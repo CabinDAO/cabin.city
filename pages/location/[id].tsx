2c99a558014c507e385231c64601c9106f9e49de
Alex Grintsvayg <grin@cabin.city>
2024-06-06 15:15:35 -0400

    static location page generation

--- pages/location/[id].tsx
+++ pages/location/[id].tsx
@@ -1,61 +1,60 @@
+import { GetStaticPaths, GetStaticProps, InferGetStaticPropsType } from 'next'
+import Error from 'next/error'
 import React from 'react'
-import { LocationPageView } from '@/components/neighborhoods/LocationPageView'
-import { AppHead } from '@/components/shared/head'
-import { getImageUrlByIpfsHash } from '@/lib/image'
 import { prisma } from '@/lib/prisma'
+import {
+  LocationFragment,
+  LocationQueryInclude,
+  LocationWithRelations,
+} from '@/utils/types/location'
+import { locationToFragment } from '@/pages/api/v2/location/list'
+import { AppHead } from '@/components/shared/head'
+import { BaseLayout } from '@/components/core/BaseLayout'
+import { LocationView } from '@/components/neighborhoods/LocationView'
+import { getImageUrlByIpfsHash } from '@/lib/image'
 
-interface LocationPageProps {
-  location: {
-    externId: string
-    title: string
-    description: string
-    image: string
+export default function LocationPage({
+  location,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  if (!location) {
+    return <Error statusCode={404} />
   }
-}
 
-const LocationPage = ({ location }: LocationPageProps) => {
   return (
     <>
       <AppHead
-        title={location.title}
+        title={location.name}
         description={location.description}
-        imageUrl={location.image}
+        imageUrl={
+          getImageUrlByIpfsHash(location.bannerImageIpfsHash, true) || undefined
+        }
         pathname={`/location/${location.externId}`}
       />
-      <LocationPageView />
+      <BaseLayout>
+        <LocationView location={location} />
+      </BaseLayout>
     </>
   )
 }
 
-export async function getServerSideProps({
-  params,
-}: {
-  params: { id: string }
-}) {
+export const getStaticPaths = (async () => {
+  return {
+    paths: [],
+    fallback: true, // or false or "blocking"
+  }
+}) satisfies GetStaticPaths
+
+export const getStaticProps = (async (context) => {
   const location = await prisma.location.findUnique({
-    where: {
-      externId: params.id,
-    },
-    select: {
-      externId: true,
-      name: true,
-      bannerImageIpfsHash: true,
-    },
+    where: { externId: context.params?.id as string },
+    include: LocationQueryInclude({ countActiveEventsOnly: true }),
   })
 
-  return location
-    ? {
-        props: {
-          location: {
-            externId: location.externId,
-            title: location.name,
-            image: getImageUrlByIpfsHash(location.bannerImageIpfsHash, true),
-          },
-        } as LocationPageProps,
-      }
-    : {
-        notFound: true,
-      }
-}
-
-export default LocationPage
+  return {
+    props: {
+      location: location
+        ? locationToFragment(location as LocationWithRelations)
+        : null,
+    },
+  }
+}) satisfies GetStaticProps<{ location: LocationFragment | null }>
