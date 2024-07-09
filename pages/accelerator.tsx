454ad871a8d24d32e634d85c195ce0f13509997c
Alex Grintsvayg <grin@cabin.city>
2024-07-09 15:04:58 -0400

    better preview

--- pages/accelerator.tsx
+++ pages/accelerator.tsx
@@ -1,5 +1,18 @@
+import React from 'react'
+import { AppHead } from '@/components/head'
 import { AcceleratorPageView } from '@/components/landing/AcceleratorPageView'
+import previewImg from '@/components/landing/accelerator-header.jpg'
 
 export default function Accelerator() {
-  return <AcceleratorPageView />
+  return (
+    <>
+      <AppHead
+        title={'Upgrade your neighborhood'}
+        description={`Cabin's Neighborhood Accelerator supports people in building vibrant communities in their local neighborhoods`}
+        imageUrl={previewImg.src}
+        pathname={'/accelerator'}
+      />
+      <AcceleratorPageView />
+    </>
+  )
 }
