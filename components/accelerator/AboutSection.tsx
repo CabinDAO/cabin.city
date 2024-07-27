d6ee4222f93abbee3c9bc1bf9c821226728a240c
Alex Grintsvayg <grin@cabin.city>
2024-07-27 14:08:07 -0400

    add image to about section

--- components/accelerator/AboutSection.tsx
+++ components/accelerator/AboutSection.tsx
@@ -1,80 +1,91 @@
 import styled from 'styled-components'
 import { Body1, H4 } from '@/components/core/Typography'
 import { AcceleratorSectionTitle } from '@/components/accelerator/AcceleratorPageView'
+import { AutoImage } from '@/components/core/AutoImage'
+import img from '@/components/accelerator/accel-imgs.jpg'
+import { useWindowSize } from 'react-use'
 
 export const AboutSection = () => {
+  const { width } = useWindowSize()
   return (
-    <>
-      <AcceleratorSectionTitle>
-        The Cabin Neighborhood Accelerator
-      </AcceleratorSectionTitle>
-
-      <TopSection>
-        <strong>
-          A 10-week program that helps people build a sense of community in
-          their neighborhoods.
-        </strong>{' '}
-        Whether you’re just starting out or want to take your community to the
-        next level, we want to help you turn your neighborhood into a thriving
-        community.
-      </TopSection>
-
+    <Container width={width}>
+      <Title>The Cabin Neighborhood Accelerator</Title>
       <Content>
-        <Box>
-          <BoxTitle>Step-by-Step Support</BoxTitle>
-          <BoxBody>
-            You’ll receive clear, personalized guidance and hands-on support
-            every step of the way.
-          </BoxBody>
-        </Box>
-        <Box>
-          <BoxTitle>Mentorship</BoxTitle>
-          <BoxBody>
-            You’ll learn from experienced mentors who have successfully built
-            vibrant communities. They’ll share what’s worked and coach you
-            through whatever comes up.
-          </BoxBody>
-        </Box>
-        <Box>
-          <BoxTitle>Incredible Humans</BoxTitle>
-          <BoxBody>
-            This extraordinary network offers a deep sense of camaraderie,
-            encouragement, brainstorming, and cross-pollination of ideas that
-            will elevate your community-building efforts. Together, you'll
-            navigate challenges, celebrate wins, and draw inspiration from the
-            collective wisdom of this exceptional group.
-          </BoxBody>
-        </Box>
-        <Box>
-          <BoxTitle>Accountability</BoxTitle>
-          <BoxBody>
-            You’ll be held accountable to take specific, time-tested actions
-            each week to build a neighborhood you’re proud of.
-          </BoxBody>
-        </Box>
-        <Box>
-          <BoxTitle>Funding Opportunities</BoxTitle>
-          <BoxBody>
-            We connect neighborhood stewards with grants and public goods
-            funding opportunities.
-          </BoxBody>
-        </Box>
+        <Left>
+          <TopSection>
+            <strong>
+              A 10-week program that helps people build a sense of community in
+              their neighborhoods.
+            </strong>{' '}
+            Whether you’re just starting out or want to take your community to
+            the next level, we want to help you turn your neighborhood into a
+            thriving community.
+          </TopSection>
+
+          <Boxes>
+            <Box>
+              <BoxTitle>Step-by-Step Support</BoxTitle>
+              <BoxBody>
+                You’ll receive clear, personalized guidance and hands-on support
+                every step of the way.
+              </BoxBody>
+            </Box>
+            <Box>
+              <BoxTitle>Mentorship</BoxTitle>
+              <BoxBody>
+                You’ll learn from experienced mentors who have successfully
+                built vibrant communities. They’ll share what’s worked and coach
+                you through whatever comes up.
+              </BoxBody>
+            </Box>
+            <Box>
+              <BoxTitle>Incredible Humans</BoxTitle>
+              <BoxBody>
+                Our network of neighborhood builders offers a deep sense of
+                camaraderie, encouragement, brainstorming, and cross-pollination
+                of ideas that will elevate your community-building efforts.
+                Together, we’ll navigate challenges, celebrate wins, and draw
+                inspiration from the collective wisdom of each other’s
+                experiments.
+              </BoxBody>
+            </Box>
+            <Box>
+              <BoxTitle>Accountability</BoxTitle>
+              <BoxBody>
+                You’ll be held accountable to take specific, time-tested actions
+                each week to build a neighborhood you’re proud of.
+              </BoxBody>
+            </Box>
+            <Box>
+              <BoxTitle>Funding Opportunities</BoxTitle>
+              <BoxBody>
+                We connect neighborhood stewards with grants and public goods
+                funding opportunities.
+              </BoxBody>
+            </Box>
+          </Boxes>
+        </Left>
+
+        <Right>
+          <AutoImage src={img.src} alt={'neighborhood polaroids'} />
+        </Right>
       </Content>
-    </>
+    </Container>
   )
 }
 
-const TopSection = styled(Body1)`
-  font-size: 2rem;
-  width: 100%;
-  margin-bottom: 2rem;
+const Container = styled.div<{ width: number }>`
+  display: flex;
+  flex-direction: column;
+  gap: 4rem;
 
   ${({ theme }) => theme.bp.md} {
-    width: 45rem;
+    width: 55rem;
   }
 
   ${({ theme }) => theme.bp.lg} {
-    width: 70rem;
+    width: auto;
+    max-width: ${({ width }) => (width > 1320 ? '1100' : width - 220)}px;
   }
 `
 
@@ -83,15 +94,49 @@ const Content = styled.div`
   width: 100%;
   flex-direction: column;
   gap: 2.4rem;
-  margin-bottom: 4rem;
-
-  ${({ theme }) => theme.bp.md} {
-    width: 55rem;
-  }
 
   ${({ theme }) => theme.bp.lg} {
-    width: 80rem;
+    flex-direction: row;
+    justify-content: space-between;
   }
+`
+
+const Left = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+
+  ${({ theme }) => theme.bp.lg} {
+    width: 48%;
+  }
+`
+
+const Right = styled.div`
+  width: 100%;
+
+  ${({ theme }) => theme.bp.lg} {
+    width: 48%;
+  }
+`
+
+const Title = styled(AcceleratorSectionTitle)`
+  ${({ theme }) => theme.bp.lg} {
+    width: auto;
+  }
+`
+
+const TopSection = styled(Body1)`
+  font-size: 2rem;
+  width: 100%;
+  margin-bottom: 2rem;
+`
+
+const Boxes = styled.div`
+  display: flex;
+  width: 100%;
+  flex-direction: column;
+  gap: 2.4rem;
+  margin-bottom: 4rem;
 `
 
 const Box = styled.div`
@@ -100,33 +145,20 @@ const Box = styled.div`
   align-items: flex-start;
   justify-content: center;
   width: 100%;
-  gap: 2.4rem;
+  gap: 1.6rem;
   background-color: white;
   border: solid 1px #000;
   box-shadow: 4px 4px 0px 0px #000;
   padding: 2rem;
-
-  ${({ theme }) => theme.bp.md} {
-    flex-direction: row;
-    padding: 6rem;
-  }
 `
 
 const BoxTitle = styled(H4)`
   font-size: 3.2rem;
   line-height: 120%; /* 3.84rem */
   font-weight: 600;
-
-  ${({ theme }) => theme.bp.md} {
-    width: 50%;
-  }
 `
 
 const BoxBody = styled(Body1)`
   opacity: 0.75;
   line-height: 1.5;
-
-  ${({ theme }) => theme.bp.md} {
-    width: 50%;
-  }
 `
