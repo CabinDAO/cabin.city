bc59dc291c7fe86f24274c9b711ba3b7a31fb2e8
hillisj <jon@cabin.city>
2024-08-19 13:51:32 -0400

    remove citizenship vouch button from profile

--- components/profile/view-profile/ProfileHeaderButton.tsx
+++ components/profile/view-profile/ProfileHeaderButton.tsx
@@ -1,57 +1,30 @@
 import { useProfile } from '@/components/auth/useProfile'
-import { useModal } from '@/components/hooks/useModal'
 import { useRouter } from 'next/router'
 import { Button } from '../../core/Button'
-import Icon from '../../core/Icon'
-import { VouchModal } from './VouchModal'
 import styled from 'styled-components'
-import { ProfileFragment, CitizenshipStatus } from '@/utils/types/profile'
+import { ProfileFragment } from '@/utils/types/profile'
 
 interface ProfileHeaderButtonProps {
   profile: ProfileFragment
   refetchProfile: VoidFunction
 }
 
-export const ProfileHeaderButton = ({
-  profile,
-  refetchProfile,
-}: ProfileHeaderButtonProps) => {
+export const ProfileHeaderButton = ({ profile }: ProfileHeaderButtonProps) => {
   const router = useRouter()
   const { user } = useProfile()
-  const { showModal } = useModal()
 
   if (!user) return null
 
   const isOwnProfile = user.externId === profile?.externId
 
   const handleProfileHeaderButtonClick = () => {
-    if (isOwnProfile) {
-      router.push(`/profile/${profile?.externId}/edit`).then()
-    } else if (profile) {
-      showModal(() => (
-        <VouchModal profile={profile} refetchProfile={refetchProfile} />
-      ))
-    }
+    router.push(`/profile/${profile?.externId}/edit`).then()
   }
 
   if (isOwnProfile && user.isProfileSetupFinished) {
     return (
       <StyledButton variant="tertiary" onClick={handleProfileHeaderButtonClick}>
         Edit Profile
-      </StyledButton>
-    )
-  } else if (
-    user.citizenshipStatus === CitizenshipStatus.Verified &&
-    (!profile?.citizenshipStatus ||
-      profile.citizenshipStatus === CitizenshipStatus.VouchRequested)
-  ) {
-    return (
-      <StyledButton
-        variant="tertiary"
-        onClick={handleProfileHeaderButtonClick}
-        startAdornment={<Icon name="thumb-up" size={1.7} />}
-      >
-        Vouch
       </StyledButton>
     )
   } else {
