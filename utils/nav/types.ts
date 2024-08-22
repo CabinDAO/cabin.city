684352820747e1b408e9849e117cf43ca8170c9f
grin <grin@cabin.city>
2024-08-22 09:34:25 -0400

    hide census while i work on it

--- utils/nav/types.ts
+++ utils/nav/types.ts
@@ -39,7 +39,7 @@ export const MenuItems: Record<MenuItemName, Config> = {
     path: '/census',
     displayText: 'Census',
     iconSize: 2.5,
-    visibility: 'always',
+    visibility: 'authOnly',
   },
   profile: {
     icon: 'profile',
