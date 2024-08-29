1e8be72fa5d7572d1c2f2d6a38d05b9c640e73cb
grin <grin@cabin.city>
2024-08-29 06:51:48 -0400

    add two new badges

---
+++ prisma/migrations/20240829104022_add_new_stamps/migration.sql
@@ -0,0 +1,2 @@
+INSERT INTO "BadgeSpec" (name, description, "updatedAt", "otterspaceSpecId", image)
+  VALUES ('Joined Cabin 2024', '', NOW(), 'join2024', ''), ('State of the Network - August 2024', '', now(), 'sotn2024', '')
