13f44d965b02dcb88c567d9523d13a17e4fa133d
grin <grin@cabin.city>
2024-09-12 21:25:25 -0400

    clarify joined activity description

--- components/core/post/profileCreatedSlots.tsx
+++ components/core/post/profileCreatedSlots.tsx
@@ -1,7 +1,7 @@
 import { Body1 } from '../Typography'
 import { PostSlots } from './post-slots'
 
-const ProfileCreatedContent = () => <Body1>Joined cabin</Body1>
+const ProfileCreatedContent = () => <Body1>Created a Cabin profile</Body1>
 
 export const profileCreatedSlots: PostSlots = {
   Content: ProfileCreatedContent,
