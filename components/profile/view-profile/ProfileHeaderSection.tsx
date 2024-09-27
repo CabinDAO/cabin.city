939e2a0367d00938cb418d15761e1d0f663e1360
grin <grin@cabin.city>
2024-09-27 15:04:22 -0400

    show quad voting power next to cabin balance

--- components/profile/view-profile/ProfileHeaderSection.tsx
+++ components/profile/view-profile/ProfileHeaderSection.tsx
@@ -1,13 +1,15 @@
+import Link from 'next/link'
+import { EXTERNAL_LINKS } from '@/utils/external-links'
 import { shortenedAddress } from '@/utils/display-utils'
+import { useDeviceSize } from '@/components/hooks/useDeviceSize'
+import useEns from '@/components/hooks/useEns'
+import { ProfileFragment } from '@/utils/types/profile'
 import styled from 'styled-components'
+import { H1, Subline2 } from '../../core/Typography'
 import { Avatar } from '@/components/profile/Avatar'
 import { ContentCard } from '../../core/ContentCard'
 import { CopyToClipboard } from '../../core/CopyToClipboard'
-import { H1, Subline2 } from '../../core/Typography'
-import useEns from '@/components/hooks/useEns'
 import { ProfileHeaderButton } from './ProfileHeaderButton'
-import { useDeviceSize } from '@/components/hooks/useDeviceSize'
-import { ProfileFragment } from '@/utils/types/profile'
 
 export const ProfileHeaderSection = ({
   profile,
@@ -33,6 +35,21 @@ export const ProfileHeaderSection = ({
                 <Subline2>{`${
                   profile.cabinTokenBalanceInt ?? 0
                 } ₡ABIN`}</Subline2>
+                <Subline2>·</Subline2>
+                <Subline2>
+                  <Link
+                    href={EXTERNAL_LINKS.SNAPSHOT}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    title="Cabin uses quadratic voting on Snapshot"
+                  >
+                    {`${
+                      profile.cabinTokenBalanceInt
+                        ? Math.sqrt(profile.cabinTokenBalanceInt).toFixed(0)
+                        : 0
+                    } vote power`}
+                  </Link>
+                </Subline2>
                 <Subline2>·</Subline2>
                 <CopyToClipboard text={ens ?? profile.wallet.address ?? ''}>
                   <Subline2>
