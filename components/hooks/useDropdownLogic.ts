ca2f8aa869b8f546ea1b7fa2134734559f716362
grin <grin@cabin.city>
2024-10-17 18:14:34 -0400

    put debounce in own file

--- components/hooks/useDropdownLogic.ts
+++ components/hooks/useDropdownLogic.ts
@@ -1,4 +1,5 @@
 import { MutableRefObject, useEffect, useRef, useState } from 'react'
+import { debounce } from '@/utils/debounce'
 import { IconName } from '@/components/core/Icon'
 
 export interface SelectOption {
@@ -89,20 +90,6 @@ const useDropdownLogic = (
     } else {
       setOpen(true)
       selectionRef.current?.focus()
-    }
-  }
-
-  const debounce = function <T extends (...args: any[]) => any>(
-    func: T,
-    delay: number
-  ): (...args: Parameters<T>) => void {
-    let timeoutId: NodeJS.Timeout
-
-    return function (...args: Parameters<T>) {
-      clearTimeout(timeoutId)
-      timeoutId = setTimeout(() => {
-        func(...args)
-      }, delay)
     }
   }
 
---
+++ utils/debounce.ts
@@ -0,0 +1,13 @@
+export const debounce = function <T extends (...args: any[]) => any>(
+  func: T,
+  delay: number
+): (...args: Parameters<T>) => void {
+  let timeoutId: NodeJS.Timeout
+
+  return function (...args: Parameters<T>) {
+    clearTimeout(timeoutId)
+    timeoutId = setTimeout(() => {
+      func(...args)
+    }, delay)
+  }
+}
