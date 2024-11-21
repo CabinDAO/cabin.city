780a36a2fccad9d3bf5ba36beef25670766652cc
grin <grin@cabin.city>
2024-11-20 21:36:27 -0500

    add wallet login page

---
+++ pages/walletlogin.tsx
@@ -0,0 +1,40 @@
+import { usePrivy } from '@privy-io/react-auth'
+import styled from 'styled-components'
+import { ContentCard } from '@/components/core/ContentCard'
+import { BaseLayout } from '@/components/core/BaseLayout'
+import { Button } from '@/components/core/Button'
+import { Body1 } from '@/components/core/Typography'
+
+export default function Page() {
+  const { user, login } = usePrivy()
+
+  if (user) {
+    console.log('user', user)
+  }
+
+  return (
+    <BaseLayout>
+      <StyledContentCard>
+        {user ? (
+          <Body1>
+            Log out first before logging in with an external wallet.
+          </Body1>
+        ) : (
+          <Button onClick={() => login({ loginMethods: ['wallet'] })}>
+            Login with Wallet
+          </Button>
+        )}
+      </StyledContentCard>
+    </BaseLayout>
+  )
+}
+
+const StyledContentCard = styled(ContentCard)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 4rem;
+  width: 100%;
+  padding: 1.6rem;
+`
